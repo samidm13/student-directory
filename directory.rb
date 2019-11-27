9e4475e155683ca1c1f75bb1ea4998165466fd9c
Samantha Dalzell Matthew <samidm13@gmail.com>
2019-11-27 15:29:20 +0000

    defined methods for header, footer and printing student names

--- directory.rb
+++ directory.rb
@@ -13,10 +13,23 @@ students = [
   "Norman Bates"
 ]
 # and then print them
-puts "The students of Villains Academy"
-puts "------------"
-students.each do |student|
-  puts student
+def print_header
+  puts "The students of Villains Academy"
+  puts "------------"
 end
+
+def print(students)
+  students.each do |student|
+    puts student
+  end
+end
+
 #finally, we print the total
-puts "Overall, we have #{students.count} great students"
+
+def print_footer(students)
+  puts "Overall, we have #{students.count} great students"
+end
+
+print_header
+print(students)
+print_footer(students)
